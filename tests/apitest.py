6647f95ec5dd83daabfb755d9c5b07c6c362d1c8
Kristoffer Andersson <kristoffer.andersson@gu.se>
2018-11-14 13:16:25 +0100

    refactor tests

--- tests/apitest.py
+++ tests/apitest.py
@@ -4,7 +4,7 @@ from karp.app import create_app
 URL = 'http://localhost:5000'
 
 
-class APITestBaseCase(unittest.TestCase):
+class TestBaseCase(unittest.TestCase):
 
     def setUp(self) -> None:
         app = create_app({
