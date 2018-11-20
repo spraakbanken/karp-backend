9b19908e49c9e0d954bc41ba217c500ab08f824f
Kristoffer Andersson <kristoffer.andersson@gu.se>
2018-11-20 14:59:46 +0100

    expanding query stub

--- src/karp/routes/query.py
+++ src/karp/routes/query.py
@@ -9,6 +9,10 @@ from flask import Blueprint, jsonify    # pyre-ignore
 query_api = Blueprint('query_api', __name__)
 
 
-@query_api.route('/query', methods=['GET'])
-def perform_query():
+@query_api.route('/<resources>/query', methods=['GET'])
+@query_api.route('/query/<resources>', methods=['GET'])
+def query(resources):
+    resources = resources.split(',')
+    for resource in resources:
+        print("/query got resource: {resource}".format(resource=resource))
     return jsonify({'status': 'ok'}), 200
