c073211add54040d75a01b34a551c25aea3e11e7
Kristoffer Andersson <kristoffer.andersson@gu.se>
2018-11-20 09:31:33 +0100

    refactor tests to use pytest fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,45 @@
+import pytest
+
+from karp import create_app, db
+from karp.models import create_new_resource, publish_resource
+
+
+class TestConfig:
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    TESTING = True
+
+
+@pytest.fixture
+def app():
+    app = create_app(TestConfig)
+    with app.app_context():
+        yield app
+
+        db.session.remove()
+        db.drop_all()
+
+
+@pytest.fixture
+def app_with_data(app):
+    with app.app_context():
+        create_new_resource('tests/data/config/places.json')
+        create_new_resource('tests/data/config/municipalities.json')
+        publish_resource('places', 1)
+        publish_resource('municipalities', 1)
+
+    return app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def client_with_data(app_with_data):
+    return app_with_data.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
