28318b371ce9c5dbefce7405af2dc632b802b9f0
Kristoffer Andersson <kristoffer.andersson@gu.se>
2018-11-21 15:05:21 +0100

    add validation stub

--- tests/test_query_api.py
+++ tests/test_query_api.py
@@ -3,3 +3,8 @@
 def test_query(client):
     response = client.get('/places/query')
     assert response.status == '200 OK'
+
+
+def test_protected(client):
+    response = client.get('/protected/query')
+    assert response.status == '403 FORBIDDEN'
