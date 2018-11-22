9ebd2a1c8f340d1ae73377d776ca002609b92ce6
Maria Öhrman <maria.ohrman@gu.se>
2018-11-22 15:44:44 +0100

    start elasticsearch in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest  # pyre-ignore
-
+import os
+import subprocess
+import tempfile
 from karp import create_app, db
 from karp.config import Config
 from karp.models import create_new_resource, publish_resource
@@ -8,6 +10,7 @@ from karp.models import create_new_resource, publish_resource
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
     TESTING = True
+    ELASTICSEARCH_URL = 'http://localhost:9200'
 
 
 @pytest.fixture
@@ -46,3 +49,35 @@ def client_with_data(app_with_data):
 @pytest.fixture
 def runner(app):
     return app.test_cli_runner()
+
+
+@pytest.fixture
+def es():
+    if not os.environ.get('ES_PATH'):
+        raise RuntimeError('must set $ES_PATH to run tests that use elasticsearch')
+    executable = os.path.join(os.environ.get('ES_PATH') + 'bin/elasticsearch')
+    data_arg = '-Epath.data=%s' % tempfile.mkdtemp()
+    logs_arg = '-Epath.logs=%s' % tempfile.mkdtemp()
+    env_copy = os.environ.copy()
+    env_copy['ES_JAVA_OPTS'] = '-Xms512m -Xmx512m'
+
+    p = subprocess.Popen([executable, data_arg, logs_arg], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    line = ''
+    while True:
+        out = p.stdout.read(1)
+        if out == b'' and p.poll() is not None:
+            raise RuntimeError('Failed to start Elasticsearch')
+        if out:
+            char = out.decode()
+            if char != '\n':
+                line += char
+            else:
+                line = ''
+
+        if 'started' in line:
+            break
+
+    yield 'we must yield something?'
+
+    p.kill()
