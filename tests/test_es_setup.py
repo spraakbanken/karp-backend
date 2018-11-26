32d13f3a737952068f803afbb8746e868c778e41
Maria Öhrman <maria.ohrman@gu.se>
2018-11-26 14:17:02 +0100

    add tests

---
+++ tests/test_es_setup.py
@@ -0,0 +1,10 @@
+import json
+import urllib.request
+import urllib.parse
+
+
+def test_es_setup(es):
+    url = 'http://localhost:9200'
+    f = urllib.request.urlopen(url)
+    answer = json.loads(f.read().decode('utf-8'))
+    assert answer['tagline'] == 'You Know, for Search'
