177c27f3a44e4b5cf9130c9d22033535dab929ef
Kristoffer Andersson <kristoffer.andersson@gu.se>
2018-11-26 09:26:00 +0100

    add string to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,42 @@
+import io
+
 import pytest  # pyre-ignore
 
 from karp import create_app, db
 from karp.config import Config
 from karp.models import create_new_resource, publish_resource
+
+
+CONFIG_PLACES="""{
+  "resource_id": "places",
+  "resource_name": "Platser i Sverige",
+  "fields": {
+    "name": {
+      "type": "string",
+      "required": true
+    },
+    "municipality": {
+      "collection": true,
+      "type": "string",
+      "required": true
+    },
+    "population": {
+      "type": "number"
+    },
+    "area": {
+      "type": "number"
+    },
+    "density": {
+      "type": "number"
+    },
+    "code": {
+      "type": "number",
+      "required": true
+    }
+  },
+  "sort": "name",
+  "id": "code"
+}"""
 
 
 class TestConfig(Config):
@@ -23,12 +57,14 @@ def app():
 @pytest.fixture
 def app_with_data(app):
     with app.app_context():
-        with open('tests/data/config/places.json') as fp:
-            create_new_resource(fp)
-        with open('tests/data/config/municipalities.json') as fp:
-            create_new_resource(fp)
+        create_new_resource(io.StringIO(CONFIG_PLACES))
         publish_resource('places', 1)
-        publish_resource('municipalities', 1)
+        # with open('tests/data/config/places.json') as fp:
+        #     create_new_resource(fp)
+        # with open('tests/data/config/municipalities.json') as fp:
+        #     create_new_resource(fp)
+        # publish_resource('places', 1)
+        # publish_resource('municipalities', 1)
 
     return app
 
