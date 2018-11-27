3ea3d6efac20c8d93c4f4e1231dc4ba23ad34ea6
Kristoffer Andersson <kristoffer.andersson@gu.se>
2018-11-27 14:51:52 +0100

    refactor check_database_status to system_monitor module

--- src/karp/database.py
+++ src/karp/database.py
@@ -1,4 +1,3 @@
-from typing import Tuple
 import fastjsonschema  # pyre-ignore
 import json
 
@@ -57,17 +56,3 @@ def get_entry(resource, entry_id, version=None):
     cls = resource_classes[resource]
     entry = cls.query.filter_by(id=entry_id).first()
     return entry
-
-
-def check_database_status() -> Tuple[bool, str]:
-    is_database_working = True
-    output = 'database is ok'
-
-    try:
-        # to check database we will execute raw query
-        db.engine.execute('SELECT 1')
-    except Exception as e:
-        output = str(e)
-        is_database_working = False
-
-    return is_database_working, output
---
+++ src/karp/system_monitor.py
@@ -0,0 +1,17 @@
+from typing import Tuple
+
+from karp import db
+
+
+def check_database_status() -> Tuple[bool, str]:
+    is_database_working = True
+    output = 'database is ok'
+
+    try:
+        # to check database we will execute raw query
+        db.engine.execute('SELECT 1')
+    except Exception as e:
+        output = str(e)
+        is_database_working = False
+
+    return is_database_working, output
