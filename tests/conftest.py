ecf363cf165c7bf55fae0dc9e6675d21e666d31d
Maria Öhrman <maria.ohrman@gu.se>
2018-11-27 11:44:45 +0100

    Revert "add tests"
    This reverts commit 32d13f3a737952068f803afbb8746e868c778e41.

--- tests/conftest.py
+++ tests/conftest.py
@@ -44,7 +44,7 @@ CONFIG_PLACES="""{
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
     TESTING = True
-    ELASTICSEARCH_URL = ['http://localhost:9200']
+    ELASTICSEARCH_URL = 'http://localhost:9200'
 
 
 @pytest.fixture
@@ -85,35 +85,3 @@ def client_with_data(app_with_data):
 @pytest.fixture
 def runner(app):
     return app.test_cli_runner()
-
-
-@pytest.fixture
-def es():
-    if not os.environ.get('ES_PATH'):
-        raise RuntimeError('must set $ES_PATH to run tests that use elasticsearch')
-    executable = os.path.join(os.environ.get('ES_PATH') + 'bin/elasticsearch')
-    data_arg = '-Epath.data=%s' % tempfile.mkdtemp()
-    logs_arg = '-Epath.logs=%s' % tempfile.mkdtemp()
-    env_copy = os.environ.copy()
-    env_copy['ES_JAVA_OPTS'] = '-Xms512m -Xmx512m'
-
-    p = subprocess.Popen([executable, data_arg, logs_arg], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    line = ''
-    while True:
-        out = p.stdout.read(1)
-        if out == b'' and p.poll() is not None:
-            raise RuntimeError('Failed to start Elasticsearch')
-        if out:
-            char = out.decode()
-            if char != '\n':
-                line += char
-            else:
-                line = ''
-
-        if 'started' in line:
-            break
-
-    yield 'we must yield something?'
-
-    p.kill()
