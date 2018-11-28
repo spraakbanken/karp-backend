d9e0893e0f7624b992d70a265f157f20353c12fa
Kristoffer Andersson <kristoffer.andersson@gu.se>
2018-11-28 10:25:32 +0100

    add correct code

--- src/karp/resourcemgr/__init__.py
+++ src/karp/resourcemgr/__init__.py
@@ -115,18 +115,38 @@ def get_entries(resource, version=None):
     return entries
 
 
-def add_entry(resource, entry, version=None):
-    add_entries(resource, [entry], version=version)
+def add_entry(resource_id, entry):
+    # TOO add to which version?
+    resource_def = Resources.query.filter_by(resource_id=resource_id, active=True).first()
+    version = resource_def.version
+    add_entries(resource_id, version, [entry])
 
 
-def add_entries(resource, entries, version=None):
-    cls = resource_classes[resource]
+def add_entries(resource_id, version, entries):
+    cls = resource_classes[resource_id]
 
-    # TODO get schema for this resource and validate entry
+    resource_def = Resources.query.filter_by(resource_id=resource_id, version=version).first()
+    try:
+        schema = json.loads(resource_def.entry_json_schema)
+        validate_entry = fastjsonschema.compile(schema)
+    except fastjsonschema.JsonSchemaDefinitionException as e:
+        raise RuntimeError(e)
+
     for entry in entries:
+        try:
+            validate_entry(entry)
+        except fastjsonschema.JsonSchemaException as e:
+            raise RuntimeError(e)
+
+        # TODO tmp fix for collections until we have decided how to handle them
+        for field_name, field_val in entry.items():
+            if isinstance(field_val, list):
+                entry[field_name] = str(field_val)
+
         new_entry = cls(**entry)
+        print(new_entry)
         db.session.add(new_entry)
-    db.session.commit()
+        db.session.commit()
 
 
 def delete_entry(resource, entry_id, version=None):
