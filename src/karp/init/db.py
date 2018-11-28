82b749c3cdfbe7de811ac7001dc2ddcec6779f0c
Maria Öhrman <maria.ohrman@gu.se>
2018-11-28 16:09:16 +0100

    fix init_db error when resources-table already exists

--- src/karp/init/db.py
+++ src/karp/init/db.py
@@ -6,7 +6,7 @@ from karp.resourcemgr import setup_resource_classes
 def init_db(app):
     db.init_app(app)
     with app.app_context():
-        if not db.engine.dialect.has_table(db.engine, 'resource'):
+        if not db.engine.dialect.has_table(db.engine, 'resources'):
             Resources.__table__.create(db.engine)
         if app.config.get('SETUP_DATABASE', True):
             setup_resource_classes()
