6a2a4fe8dea8fcde00567029b2438435c14000aa
Kristoffer Andersson <kristoffer.andersson@gu.se>
2018-12-03 13:24:14 +0100

    add todo note

--- src/karp/init/db.py
+++ src/karp/init/db.py
@@ -2,7 +2,7 @@ from karp.database import db
 from karp.database import ResourceDefinition
 from karp.resourcemgr import setup_resource_classes
 
-
+# TODO: Move this function to database
 def init_db(app):
     db.init_app(app)
     with app.app_context():
