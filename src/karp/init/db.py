05f5bba12a85f61d2cb708233871c85c2b2b0a57
Maria Öhrman <maria.ohrman@gu.se>
2018-12-03 16:24:13 +0100

    create resource_defition table on startup

--- src/karp/init/db.py
+++ src/karp/init/db.py
@@ -2,11 +2,12 @@ from karp.database import db
 from karp.database import ResourceDefinition
 from karp.resourcemgr import setup_resource_classes
 
+
 # TODO: Move this function to database
 def init_db(app):
     db.init_app(app)
     with app.app_context():
-        if not db.engine.dialect.has_table(db.engine, 'resources'):
+        if not db.engine.dialect.has_table(db.engine, 'resource_definition'):
             ResourceDefinition.__table__.create(db.engine)
         if app.config.get('SETUP_DATABASE', True):
             setup_resource_classes()
