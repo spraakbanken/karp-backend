f2e1ca02039b526449c26679aa1f3127e84408e0
Maria Öhrman <maria.ohrman@gu.se>
2018-12-04 12:53:04 +0100

    better interface handling

--- src/karp/search/search.py
+++ src/karp/search/search.py
@@ -1,30 +1,3 @@
-from elasticsearch_dsl import Search, Q
-
-
-class KarpSearch:
-
-    def __init__(self):
-        self.impl = SearchInterface()
-
-    def init(self, impl):
-        self.impl = impl
-
-    def get_query(self, *args, **kwargs):
-        return self.impl.get_query(*args, **kwargs)
-
-    def search(self, *args, **kwargs):
-        return self.impl.search(*args, **kwargs)
-
-    def create_index(self, *args, **kwargs):
-        return self.impl.create_index(*args, **kwargs)
-
-    def publish_index(self, *args, **kwargs):
-        return self.impl.publish_index(*args, **kwargs)
-
-    def add_entries(self, *args, **kwargs):
-        self.impl.add_entries(*args, **kwargs)
-
-
 class SearchInterface:
 
     def get_query(self, resources):
@@ -41,3 +14,27 @@ class SearchInterface:
 
     def add_entries(self, resource_id, created_db_entries):
         return
+
+
+class KarpSearch(SearchInterface):
+
+    def __init__(self):
+        self.impl = SearchInterface()
+
+    def init(self, impl):
+        self.impl = impl
+
+    def get_query(self, resources):
+        return self.impl.get_query(resources)
+
+    def search(self, resources, query=None):
+        return self.impl.search(resources, query=query)
+
+    def create_index(self, resource_id, config):
+        return self.impl.create_index(resource_id, config)
+
+    def publish_index(self, alias_name, index_name):
+        return self.impl.publish_index(alias_name, index_name)
+
+    def add_entries(self, resource_id, created_db_entries):
+        self.impl.add_entries(resource_id, created_db_entries)
