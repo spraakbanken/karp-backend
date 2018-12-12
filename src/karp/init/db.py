6e411fce2bec237845744662f8930530080e44b4
Maria Öhrman <maria.ohrman@gu.se>
2018-12-12 12:47:53 +0100

    add alembic for database migration

--- src/karp/init/db.py
+++ src/karp/init/db.py
@@ -1,5 +1,4 @@
-from karp.database import db
-from karp.database import ResourceDefinition
+from karp.database import db, ResourceDefinition
 from karp.resourcemgr import setup_resource_classes
 
 
@@ -7,6 +6,8 @@ from karp.resourcemgr import setup_resource_classes
 def init_db(app):
     db.init_app(app)
     with app.app_context():
+        # TODO this must be here right now, even though alembic is in change of table creation
+        # if removed, something complains about resource_definiton not existing, even though it does!
         if not db.engine.dialect.has_table(db.engine, 'resource_definition'):
             ResourceDefinition.__table__.create(db.engine)
         if app.config.get('SETUP_DATABASE', True):
