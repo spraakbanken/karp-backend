cb7ea1ee11cc232565508917d3b9710bc8faf07b
Maria Öhrman <maria.ohrman@gu.se>
2019-01-07 13:46:41 +0100

    add new path to get entry for editing

--- src/karp/api/edit.py
+++ src/karp/api/edit.py
@@ -1,6 +1,7 @@
 from flask import Blueprint                     # pyre-ignore
 from flask import jsonify as flask_jsonify       # pyre-ignore
 from flask import request  # pyre-ignore
+from flask import current_app  # pyre-ignore
 
 import karp.resourcemgr.entrymgr as entrymgr
 
@@ -8,10 +9,16 @@ import karp.resourcemgr.entrymgr as entrymgr
 edit_api = Blueprint('edit_api', __name__)
 
 
+@edit_api.route('/<resource_id>/get/<entry_id>')
+def get_entry_for_editing(resource_id, entry_id):
+    db_entry = entrymgr.get_entry(resource_id, entry_id)
+    return current_app.response_class(db_entry.body, mimetype=current_app.config['JSONIFY_MIMETYPE'])
+
+
 @edit_api.route('/<resource_id>/add', methods=['POST'])
 def add_entry(resource_id):
     """
-    Example add: `curl -XPOST http://localhost:5000/entry?resource=places
+    Example add: `curl -XPOST http://localhost:5000/places/add
                  -d '{"name": "Göteborg", "population": 3, "area": 30000}' -H "Content-Type: application/json"`
     """
     data = request.get_json()
