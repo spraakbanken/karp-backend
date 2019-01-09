27920905f8732f28fc8a4ca03e32510067532e85
Maria Öhrman <maria.ohrman@gu.se>
2019-01-09 16:20:50 +0100

    some changes to edit API

--- src/karp/api/edit.py
+++ src/karp/api/edit.py
@@ -1,7 +1,7 @@
 from flask import Blueprint                     # pyre-ignore
 from flask import jsonify as flask_jsonify       # pyre-ignore
 from flask import request  # pyre-ignore
-from flask import current_app  # pyre-ignore
+import json
 
 import karp.resourcemgr.entrymgr as entrymgr
 
@@ -9,27 +9,21 @@ import karp.resourcemgr.entrymgr as entrymgr
 edit_api = Blueprint('edit_api', __name__)
 
 
-@edit_api.route('/<resource_id>/get/<entry_id>')
+@edit_api.route('/<resource_id>/<entry_id>')
 def get_entry_for_editing(resource_id, entry_id):
     db_entry = entrymgr.get_entry(resource_id, entry_id)
-    return current_app.response_class(db_entry.body, mimetype=current_app.config['JSONIFY_MIMETYPE'])
+    result = {
+        'id': entry_id,
+        'version': -1,  # TODO: version are only available in history right now
+        'entry': json.loads(db_entry.body)
+    }
+    return flask_jsonify(result)
 
 
 @edit_api.route('/<resource_id>/add', methods=['POST'])
 def add_entry(resource_id):
-    """
-    Example add: `curl -XPOST http://localhost:5000/places/add
-                 -d '{"name": "Göteborg", "population": 3, "area": 30000}' -H "Content-Type: application/json"`
-    """
     data = request.get_json()
-    new_id = entrymgr.add_entry(resource_id, data)
-    return flask_jsonify({'status': 'added', 'newID': new_id}), 201
-
-
-@edit_api.route('/<resource_id>/add_with_message', methods=['POST'])
-def add_entry_with_msg(resource_id):
-    data = request.get_json()
-    new_id = entrymgr.add_entry(resource_id, data['doc'], message=data['message'])
+    new_id = entrymgr.add_entry(resource_id, data['entry'], message=data['message'])
     return flask_jsonify({'status': 'added', 'newID': new_id}), 201
 
 
@@ -42,14 +36,15 @@ def get_all_entries(resource_id):
     return flask_jsonify(entries)
 
 
-@edit_api.route('/<resource_id>/update/<entry_id>', methods=['POST'])
+@edit_api.route('/<resource_id>/<entry_id>/update', methods=['POST'])
 def update_entry(resource_id, entry_id):
     data = request.get_json()
-    entrymgr.update_entry(resource_id, entry_id, data['doc'], message=data['message'])
+    version = data['version']  # TODO: use this
+    entrymgr.update_entry(resource_id, entry_id, data['entry'], message=data['message'])
     return flask_jsonify({'status': 'updated'})
 
 
-@edit_api.route('/<resource_id>/delete/<entry_id>', methods=['DELETE'])
+@edit_api.route('/<resource_id>/<entry_id>/delete', methods=['DELETE'])
 def delete_entry(resource_id, entry_id):
     entrymgr.delete_entry(resource_id, entry_id)
     return flask_jsonify({'status': 'removed'})
