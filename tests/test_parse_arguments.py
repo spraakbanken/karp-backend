787da15ce9ab08d4983a90b12f51fdf08e541058
Kristoffer Andersson <kristoffer.andersson@gu.se>
2019-01-16 14:49:03 +0100

    remove unused test

--- tests/test_parse_arguments.py
+++
@@ -1,8 +0,0 @@
-from karp.api.query import QueryParameters, parse_arguments
-
-
-def test_empty_arg():
-    args = {}
-    query_params = parse_arguments(args)
-    
-    assert isinstance(query_params, QueryParameters)
