d8e4584c69bb97bc3ce841afd1082cfe522b9130
Kristoffer Andersson <kristoffer.andersson@gu.se>
2019-01-16 15:07:48 +0100

    move stuff from api.query to karp.search

--- src/karp/search/search.py
+++ src/karp/search/search.py
@@ -1,8 +1,9 @@
 import distutils
 
-from typing import Optional, Callable, TypeVar, List
+from typing import Optional, Callable, TypeVar, List, Dict
 
 from . import errors
+from karp import util
 
 
 T = TypeVar('T', bool, int, str, List[str])
@@ -20,8 +21,45 @@ def arg_get(args,
     return convert(arg)
 
 
+def parse_arguments(args: Dict[str, str], resource_str: str = None) -> Dict:
+    params = {}
+    s2l = util.convert.str2list(',')
+    if resource_str:
+        params['resources'] = arg_get('resources', s2l)
+    else:
+        params['resources'] = s2l(resource_str)
+    available_fields = {
+        'from': {'default': 0, 'convert': int},
+        'size': {'default': 25, 'convert': int},
+        'split_results': {'default': False, 'convert': distutils.util.strtobool},
+        'lexicon_stats': {'default': True, 'convert': distutils.util.strtobool},
+        'include_fields': {'convert': s2l},
+        'exclude_fields': {'convert': s2l},
+        'format': {},
+        'format_query': {},
+        'q': {},
+        'sort': {}
+    }
+
+    for field, m in available_fields.items():
+        params[field] = arg_get(field, m.get('convert'), m.get('default'))
+
+    return params
+
+
 class Query:
-    resource = []
+    from_ = 0
+    size = 25
+    split_results = False
+    lexicon_stats = True
+    include_fields = None
+    exclude_fields = None
+    fields = []
+    format = None
+    format_query = None
+    q = None
+    resources = []
+    sort = ""
 
     def __init__(self):
         pass
@@ -30,12 +68,42 @@ class Query:
         if resource_str is None:
             raise errors.IncompleteQuery('No resources are defined.')
         self.resources = resource_str.split(',')
+        self.from_ = arg_get(args, 'from', int, 0)
+        self.size = arg_get(args, 'size', int, 25)
         self.split_results = arg_get(args, 'split_results', distutils.util.strtobool, False)
+        self.lexicon_stats = arg_get(args, 'lexicon_stats', distutils.util.strtobool, True)
+        self.include_fields = arg_get(args, 'include_fields', util.convert.str2list(','))
+        self.exclude_fields = arg_get(args, 'exclude_fields', util.convert.str2list(','))
+        self.format = arg_get(args, 'format')
+        self.format_query = arg_get(args, 'format_query')
+        self.q = arg_get(args, 'q')
+        self.sort = arg_get(args, 'sort')
 
     def __repr__(self) -> str:
-        return '{} resources={} split_results={}'.format(self._self_name(),
-                                                         self.resources,
-                                                         self.split_results)
+        return """
+            {}(
+                q={}
+                resources={}
+                include_fields={}
+                exclude_fields={}
+                fields={}
+                sort={}
+                from={}, size={},
+                split_results={}, lexicon_stats={},
+                format={}
+                format_query={}
+                resource_params={}
+            )""".format(self._self_name(),
+                        self.q,
+                        self.resources,
+                        self.include_fields,
+                        self.exclude_fields,
+                        self.fields if self.fields else [],
+                        self.sort,
+                        self.from_, self.size,
+                        self.split_results, self.lexicon_stats,
+                        self.format,
+                        self.format_query)
 
     def _self_name(self) -> str:
         return 'Query'
