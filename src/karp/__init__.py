6d022f62562a79b96708097803c7440a623d2380
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-03-05 12:03:04 +0100

    bump version to 0.6.4

--- src/karp/__init__.py
+++ src/karp/__init__.py
@@ -8,7 +8,7 @@ from flask_cors import CORS  # pyre-ignore
 from karp.errors import KarpError
 import karp.util.logging.slack as slack_logging
 
-__version__ = '0.6.3'
+__version__ = '0.6.4'
 
 
 # TODO handle settings correctly
