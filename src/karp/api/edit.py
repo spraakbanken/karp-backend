233e587bf0c4fa9a305e25f6797137ed01b96bd0
Maria Öhrman <maria.ohrman@gu.se>
2019-03-06 15:43:38 +0100

    update edit api and doc

--- src/karp/api/edit.py
+++ src/karp/api/edit.py
@@ -16,7 +16,7 @@ edit_api = Blueprint('edit_api', __name__)
 def add_entry(user, resource_id):
     data = request.get_json()
     new_id = entrywrite.add_entry(resource_id, data['entry'], user.identifier, message=data.get('message', ''))
-    return flask_jsonify({'status': 'added', 'newID': new_id}), 201
+    return flask_jsonify({'newID': new_id}), 201
 
 
 @edit_api.route('/<resource_id>/<entry_id>/update', methods=['POST'])
@@ -24,14 +24,14 @@ def add_entry(user, resource_id):
 def update_entry(user, resource_id, entry_id):
     data = request.get_json()
     entrywrite.update_entry(resource_id, entry_id, data['entry'], user.identifier, message=data['message'])
-    return flask_jsonify({'status': 'updated'})
+    return '', 204
 
 
 @edit_api.route('/<resource_id>/<entry_id>/delete', methods=['DELETE'])
 @auth.auth.authorization('WRITE', add_user=True)
 def delete_entry(user, resource_id, entry_id):
     entrywrite.delete_entry(resource_id, entry_id, user.identifier)
-    return flask_jsonify({'status': 'removed'})
+    return '', 204
 
 
 @edit_api.route('/<resource_id>/preview', methods=['POST'])
