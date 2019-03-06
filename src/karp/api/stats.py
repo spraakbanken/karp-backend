d64e03391e928d717a9707f0a4a2c64aa82815c4
Maria Öhrman <maria.ohrman@gu.se>
2019-03-06 11:09:29 +0100

    change stats URL and document in OpenAPI

--- src/karp/api/stats.py
+++ src/karp/api/stats.py
@@ -6,7 +6,7 @@ import karp.auth.auth as auth
 stats_api = Blueprint('stats_api', __name__)
 
 
-@stats_api.route('/stats/<resource_id>/<field>', methods=['GET'])
+@stats_api.route('/<resource_id>/stats/<field>', methods=['GET'])
 @auth.auth.authorization('READ')
 def get_field_values(resource_id, field):
     return jsonify(search.search.statistics(resource_id, field))
