951ff0e645343b1c852b885591d22b940786bd96
Maria Öhrman <maria.ohrman@gu.se>
2019-04-09 15:42:03 +0200

    add a run script that uses gevent

---
+++ run.py
@@ -0,0 +1,22 @@
+import os
+import sys
+from gevent import monkey
+monkey.patch_all()
+from gevent.pywsgi import WSGIServer
+
+from karp import create_app
+from karp.config import MariaDBConfig
+
+user = os.environ["MARIADB_USER"]
+passwd = os.environ["MARIADB_PASSWORD"]
+dbhost = os.environ["MARIADB_HOST"]
+dbname = os.environ["MARIADB_DATABASE"]
+
+try:
+    port = int(sys.argv[1])
+except (IndexError, ValueError):
+    sys.exit("Usage %s <port>" % sys.argv[0])
+
+application = create_app(MariaDBConfig(user, passwd, dbhost, dbname, True))
+
+WSGIServer(('0.0.0.0', port), application).serve_forever()
