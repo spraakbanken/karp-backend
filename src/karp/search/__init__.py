e218320e355576d826d6c50c420fbf2a2deb9ba6
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-05-28 14:41:27 +0200

    style fixes

--- src/karp/search/__init__.py
+++ src/karp/search/__init__.py
@@ -1,4 +1,4 @@
-from karp.search.search import KarpSearch, SearchInterface, Query
-from . import errors
+from karp.search.search import KarpSearch, SearchInterface, Query  # noqa: F401
+from . import errors  # noqa: F401
 
 search = KarpSearch()
