d480fc66eb6c3165cdd49e305f4e7ad69214be7a
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-06-03 13:53:53 +0200

    add get_active_or_latest

--- src/karp/database/__init__.py
+++ src/karp/database/__init__.py
@@ -120,6 +120,13 @@ def get_active_resource_definition(id: str) -> Optional[ResourceDefinition]:
     return ResourceDefinition.query.filter_by(resource_id=id, active=True).first()
 
 
+def get_active_or_latest_resource_definition(id: str) -> Optional[ResourceDefinition]:
+    rd = get_active_resource_definition(id)
+    if not rd:
+        rd = get_latest_resource_definition(id)
+    return rd
+
+
 def get_next_resource_version(id: str) -> int:
     latest_resource = get_latest_resource_definition(id)
 
