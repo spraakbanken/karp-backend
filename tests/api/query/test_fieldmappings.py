472d25ed5ae93b7e066fbaae0e57918a602af9e4
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-06-04 09:25:51 +0200

    support field_mappings for OPS
    field_mappings work for:
    - all Query Operators
    - top-level Logicals
    - OR in field, plain second arg

--- tests/api/query/test_fieldmappings.py
+++ tests/api/query/test_fieldmappings.py
@@ -11,9 +11,14 @@ def get_json(client, path):
 
 
 @pytest.mark.parametrize('resource,query,expected_hits', [
-    ('places', 'equals|state|västerbottens', 7),
+    ('places', 'equals|state|västerbottens', 6),
+    ('places,municipalities', 'equals|state|västerbottens', 7),
     ('municipalities', 'gt|population|42108', 1),
-    ('places', 'contains||or|state|name||botten', 5),
+    ('municipalities,places', 'lt|population|4133', 5),
+    ('places', 'contains||or|state|name||st', 7),
+    ('places', 'and||startswith|state|Norr||equals|area|6312', 4),
+    ('places', 'or||startswith|state|Norr||equals|area|6312', 8),
+    ('places', 'not||startswith|state|Norr||equals|area|6312', 1),
 ])
 def test_query_field_mapping(client_with_entries_scope_session, resource, query, expected_hits):
     path = '{resource}/query?{query}'.format(
