007a0ac4fd2042762026e905b3208d912b2d88f6
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-11-19 10:08:41 +0100

    update run.py

--- run.py
+++ run.py
@@ -1,22 +1,47 @@
 import os
 import sys
 from gevent import monkey
+
 monkey.patch_all()
 from gevent.pywsgi import WSGIServer
+
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=".env")
 
 from karp import create_app
 from karp.config import MariaDBConfig
 
-user = os.environ["MARIADB_USER"]
-passwd = os.environ["MARIADB_PASSWORD"]
-dbhost = os.environ["MARIADB_HOST"]
-dbname = os.environ["MARIADB_DATABASE"]
 
-try:
-    port = int(sys.argv[1])
-except (IndexError, ValueError):
-    sys.exit("Usage %s <port>" % sys.argv[0])
+def usage():
+    sys.exit(
+        """Usage:
+        {script} <port> - run in production mode on port <port>
+        {script} dev    - run in development mode
+        """.format(
+            script=sys.argv[0]
+        )
+    )
 
-application = create_app(MariaDBConfig(user, passwd, dbhost, dbname, True))
 
-WSGIServer(('0.0.0.0', port), application).serve_forever()
+USER = os.environ["MARIADB_USER"]
+PASSWD = os.environ["MARIADB_PASSWORD"]
+DBHOST = os.environ["MARIADB_HOST"]
+DBNAME = os.environ["MARIADB_DATABASE"]
+
+application = create_app(MariaDBConfig(USER, PASSWD, DBHOST, DBNAME, True))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        try:
+            PORT = int(sys.argv[1])
+            WSGIServer(("0.0.0.0", PORT), application).serve_forever()
+        except ValueError:
+            if sys.argv[1].lower() == "dev":
+                application.run(debug=True)
+            else:
+                usage()
+    else:
+        usage()
+
