e91cebb81206e3f80a2658571d4c992d221296b4
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2020-03-06 13:24:34 +0100

    use slack-log-utils and webhhoks

--- karp/util/logging/slack.py
+++ karp/util/logging/slack.py
@@ -1,8 +1,12 @@
 import logging
-from slacker_log_handler import SlackerLogHandler  # pyre-ignore
+# from slacker_log_handler import SlackerLogHandler  # pyre-ignore
+from slack_log_utils import SlackWebhookFormatter, SlackWebhookHandler
 
 
 def get_slack_logging_handler(secret):
-    slack_handler = SlackerLogHandler(secret, "#karp-tng-log", stack_trace=True)
-    slack_handler.setLevel(logging.ERROR)
+    slack_formatter = SlackWebhookFormatter()
+    slack_handler = SlackWebhookHandler(url=secret, level=logging.ERROR)
+    slack_handler.setFormatter(slack_formatter)
+    # slack_handler = SlackerLogHandler(secret, "#karp-tng-log", stack_trace=True)
+    # slack_handler.setLevel(logging.ERROR)
     return slack_handler
