fb6e4b3695650f88ca2f729e10d37624ae281f43
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2020-04-14 11:05:05 +0200

    Expand test for doc

--- tests/unit_tests/api/test_doc_api.py
+++ tests/unit_tests/api/test_doc_api.py
@@ -1,4 +1,9 @@
+
 def test_get_yaml(client):
     response = client.get("/documentation/spec.yaml")
 
     assert 200 <= response.status_code < 300
+
+    print(f"{response!r}")
+    assert b"Karp API" in response.data
+    assert b"Karp TNG" in response.data
