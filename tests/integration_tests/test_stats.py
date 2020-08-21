8599323f850314bb7ea4ab6b1d883620c126ba10
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2020-08-21 10:11:12 +0200

    test: make test pass

--- tests/integration_tests/test_stats.py
+++ tests/integration_tests/test_stats.py
@@ -4,4 +4,4 @@ from tests.utils import get_json
 def test_stats(client_with_entries_scope_session):
     entries = get_json(client_with_entries_scope_session, "places/stats/area")
 
-    assert entries is None
+    assert len(entries) == 4
