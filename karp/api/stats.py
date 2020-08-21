9a5efc8994cd2cdfddf1795d3c822f82671da359
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2020-08-21 10:10:56 +0200

    refactor: add type annotations

--- karp/api/stats.py
+++ karp/api/stats.py
@@ -8,4 +8,5 @@ stats_api = Blueprint("stats_api", __name__)
 
 @stats_api.route("/<resource_id>/stats/<field>", methods=["GET"])
 @auth.auth.authorization("READ")
+def get_field_values(resource_id: str, field: str):
     return jsonify(search.statistics(resource_id, field))
