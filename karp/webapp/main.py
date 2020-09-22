fd12bcf84c65584f08df78be0c14ddcd274b7814
Kristoffer Andersson <kod.kristoff@gmail.com>
2020-09-22 10:26:59 +0200

    refactor: add webapp main

---
+++ karp/webapp/main.py
@@ -0,0 +1,43 @@
+import logging
+
+try:
+    from importlib.metadata import entry_points
+except ImportError:
+    from importlib_metadata import entry_points
+
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+logger = logging.getLogger("karp")
+
+__version__ = "0.8.1"
+
+
+def create_app() -> FastAPI:
+    app = FastAPI(title="Karp API", redoc_url="/", version=__version__)
+
+    from karp.application import ctx
+    from karp.infrastructure.sql.sql_resource_repository import SqlResourceRepository
+
+    ctx.resource_repo = SqlResourceRepository()
+    load_modules(app)
+    from karp.errors import KarpError
+
+    @app.exception_handler(KarpError)
+    async def _karp_error_handler(request: Request, exc: KarpError):
+        return JSONResponse(
+            status_code=exc.http_return_code,
+            content={"error": exc.message, "errorCode": exc.code},
+        )
+
+    return app
+
+
+def load_modules(app=None):
+    for ep in entry_points()["karp.modules"]:
+        logger.info("Loading module: %s", ep.name)
+        mod = ep.load()
+        if app:
+            init_app = getattr(mod, "init_app", None)
+            if init_app:
+                init_app(app)
