dbe1e8076c965501939f220badea9f28fe6e9085
Kristoffer Andersson <kod.kristoff@gmail.com>
2020-09-30 18:06:45 +0200

    tests: update integration tests for resources api

--- tests/integration_tests/webapp/test_resources_api.py
+++ tests/integration_tests/webapp/test_resources_api.py
@@ -2,3 +2,8 @@ def test_get_resources(fa_client_w_places):
     response = fa_client_w_places.get("/resources")
 
     assert response.status_code == 200
+
+    response_data = response.json()
+
+    assert len(response_data) == 1
+    assert response_data[0] == {"resource_id": "places"}
