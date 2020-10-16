a80109aaac153c114aeeb9e8a55e7a8079dfb6fd
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2020-10-16 14:23:18 +0200

    tests: update network tests

--- tests/integration_tests/domain_services_network_integration_test.py
+++ tests/integration_tests/domain_services_network_integration_test.py
@@ -1,6 +1,7 @@
-from karp.domain.services.network import get_refs
+from karp.domain.services.network import get_refs, get_referenced_entries
 
 from karp.application import ctx
+from karp.application.services import entries
 
 
 def test_get_refs_for_places(
@@ -48,98 +49,109 @@ municipalities = [
 ]
 
 
-def test_no_refs_1(app_with_data_f):
-    app = app_with_data_f()
+def test_no_refs_1(
+    places_published_scope_module, municipalities_published_scope_module
+):
+    entries.add_entries("places", places[0:1], "test", resource_version=1)
 
-    with app.app_context():
-        entrywrite.add_entries("places", places[0:1], "test", resource_version=1)
-
-        referenced_entries = list(network.get_referenced_entries("places", 1, "3"))
-        assert len(referenced_entries) == 0
-
-
-def test_no_refs_2(app_with_data_f):
-    app = app_with_data_f()
-
-    with app.app_context():
-        entrywrite.add_entries("places", places[0:2], "test", resource_version=1)
-
-        referenced_entries = list(network.get_referenced_entries("places", 1, "3"))
-        assert len(referenced_entries) == 0
-        referenced_entries = list(network.get_referenced_entries("places", 1, "4"))
-        assert len(referenced_entries) == 0
+    referenced_entries = list(
+        get_referenced_entries(ctx.resource_repo, places_published_scope_module, 1, "3")
+    )
+    assert len(referenced_entries) == 0
 
 
-def test_internal_ref(app_with_data_f):
-    app = app_with_data_f()
+def test_no_refs_2(
+    places_published_scope_module, municipalities_published_scope_module
+):
+    entries.add_entries("places", places[0:2], "test", resource_version=1)
 
-    with app.app_context():
-        entrywrite.add_entries("places", places[1:3], "test", resource_version=1)
+    referenced_entries = list(
+        get_referenced_entries(ctx.resource_repo, places_published_scope_module, 1, "3")
+    )
+    assert len(referenced_entries) == 0
+    referenced_entries = list(
+        get_referenced_entries(ctx.resource_repo, places_published_scope_module, 1, "4")
+    )
+    assert len(referenced_entries) == 0
 
-        referenced_entries = list(network.get_referenced_entries("places", 1, "5"))
-        assert len(referenced_entries) == 1
-        assert referenced_entries[0]["entry_id"] == "4"
+
+def test_internal_ref(
+    places_published_scope_module, municipalities_published_scope_module
+):
+    entries.add_entries("places", places[1:3], "test", resource_version=1)
+
+    referenced_entries = list(
+        get_referenced_entries(ctx.resource_repo, places_published_scope_module, 1, "5")
+    )
+    assert len(referenced_entries) == 1
+    assert referenced_entries[0]["entry_id"] == "4"
 
 
-def test_external_ref(app_with_data_f):
-    app = app_with_data_f()
+def test_external_ref(
+    places_published_scope_module, municipalities_published_scope_module
+):
+    entries.add_entries("places", places, "test", resource_version=1)
+    entries.add_entries("municipalities", municipalities, "test", resource_version=1)
 
-    with app.app_context():
-        entrywrite.add_entries("places", places, "test", resource_version=1)
-        entrywrite.add_entries(
-            "municipalities", municipalities, "test", resource_version=1
+    referenced_entries = list(
+        get_referenced_entries(ctx.resource_repo, places_published_scope_module, 1, "3")
+    )
+    assert len(referenced_entries) == 1
+    assert referenced_entries[0]["entry_id"] == "1"
+
+    referenced_entries = list(
+        get_referenced_entries(ctx.resource_repo, places_published_scope_module, 1, "4")
+    )
+    assert 3 == len(referenced_entries)
+    ref_municipalities = [
+        entry
+        for entry in referenced_entries
+        if entry["resource_id"] == "municipalities"
+    ]
+    assert ref_municipalities[0]["entry_id"] in ["2", "3"]
+    assert ref_municipalities[1]["entry_id"] in ["2", "3"]
+    assert ref_municipalities[0]["entry_id"] != ref_municipalities[1]["id"]
+
+    referenced_entries = list(
+        get_referenced_entries(ctx.resource_repo, places_published_scope_module, 1, "5")
+    )
+    assert len(referenced_entries) == 3
+    ref_municipalities = [
+        entry
+        for entry in referenced_entries
+        if entry["resource_id"] == "municipalities"
+    ]
+    assert ref_municipalities[0]["entry_id"] in ["2", "3"]
+    assert ref_municipalities[1]["entry_id"] in ["2", "3"]
+    assert ref_municipalities[0]["entry_id"] != ref_municipalities[1]["id"]
+
+
+def test_virtual_internal_ref(
+    places_published_scope_module, municipalities_published_scope_module
+):
+
+    entries.add_entries("places", places[1:3], "test", resource_version=1)
+
+    referenced_entries = list(
+        get_referenced_entries(ctx.resource_repo, places_published_scope_module, 1, "4")
+    )
+    assert 1 == len(referenced_entries)
+    assert referenced_entries[0]["entry_id"] == "5"
+
+
+def test_virtual_external_ref(
+    places_published_scope_module, municipalities_published_scope_module
+):
+    entries.add_entries("places", places[0:1], "test", resource_version=1)
+    entries.add_entries(
+        "municipalities", municipalities[0:1], "test", resource_version=1
+    )
+
+    referenced_entries = list(
+        get_referenced_entries(
+            ctx.resource_repo, municipalities_published_scope_module, 1, "1"
         )
-
-        referenced_entries = list(network.get_referenced_entries("places", 1, "3"))
-        assert len(referenced_entries) == 1
-        assert referenced_entries[0]["entry_id"] == "1"
-
-        referenced_entries = list(network.get_referenced_entries("places", 1, "4"))
-        assert 3 == len(referenced_entries)
-        ref_municipalities = [
-            entry
-            for entry in referenced_entries
-            if entry["resource_id"] == "municipalities"
-        ]
-        assert ref_municipalities[0]["entry_id"] in ["2", "3"]
-        assert ref_municipalities[1]["entry_id"] in ["2", "3"]
-        assert ref_municipalities[0]["entry_id"] != ref_municipalities[1]["id"]
-
-        referenced_entries = list(network.get_referenced_entries("places", 1, "5"))
-        assert len(referenced_entries) == 3
-        ref_municipalities = [
-            entry
-            for entry in referenced_entries
-            if entry["resource_id"] == "municipalities"
-        ]
-        assert ref_municipalities[0]["entry_id"] in ["2", "3"]
-        assert ref_municipalities[1]["entry_id"] in ["2", "3"]
-        assert ref_municipalities[0]["entry_id"] != ref_municipalities[1]["id"]
-
-
-def test_virtual_internal_ref(app_with_data_f):
-    app = app_with_data_f()
-
-    with app.app_context():
-        entrywrite.add_entries("places", places[1:3], "test", resource_version=1)
-
-        referenced_entries = list(network.get_referenced_entries("places", 1, "4"))
-        assert 1 == len(referenced_entries)
-        assert referenced_entries[0]["entry_id"] == "5"
-
-
-def test_virtual_external_ref(app_with_data_f):
-    app = app_with_data_f()
-
-    with app.app_context():
-        entrywrite.add_entries("places", places[0:1], "test", resource_version=1)
-        entrywrite.add_entries(
-            "municipalities", municipalities[0:1], "test", resource_version=1
-        )
-
-        referenced_entries = list(
-            network.get_referenced_entries("municipalities", 1, "1")
-        )
-        assert 1 == len(referenced_entries)
-        assert referenced_entries[0]["resource_id"] == "places"
-        assert referenced_entries[0]["entry_id"] == "3"
+    )
+    assert 1 == len(referenced_entries)
+    assert referenced_entries[0]["resource_id"] == "places"
+    assert referenced_entries[0]["entry_id"] == "3"
