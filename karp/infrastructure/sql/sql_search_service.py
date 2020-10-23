03b3b164582476f25260352f58a019cef69e705f
Kristoffer Andersson <kod.kristoff@gmail.com>
2020-10-23 09:21:24 +0200

    refactor: expand stub for sql search service

--- karp/infrastructure/sql/sql_search_service.py
+++ karp/infrastructure/sql/sql_search_service.py
@@ -1,5 +1,19 @@
-from karp.domain.models.search_service import SearchService
+from typing import List, Optional
+
+from karp.domain.models.search_service import SearchService, IndexEntry
+from karp.domain.models.resource import Resource
+from karp.domain.models.entry import Entry
 
 
 class SqlSearchService(SearchService, search_service_type="sql_search_service", is_default=True):
-    pass
+    def add_entries(self, resource_id, entries: List[IndexEntry]):
+        pass
+
+    def delete_entry(
+        self,
+        resource: Resource,
+        *,
+        entry: Optional[Entry] = None,
+        entry_id: Optional[str] = None
+    ):
+        pass
