521f015904c1ce216bceac94f581447cceac06d9
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2020-12-10 13:41:52 +0100

    feat: add paradigm stub

--- karp/domain/models/morphological_entry.py
+++ karp/domain/models/morphological_entry.py
@@ -1,3 +1,7 @@
+from typing import List, Tuple
+
+from paradigmextract import morphparser
+
 from karp.domain.models.entry import Entry, EntryOp, EntryStatus
 
 from karp.utility import unique_id
@@ -6,10 +10,27 @@ from karp.utility import unique_id
 class MorphologicalEntry(Entry):
     def __init__(
         self,
-        *pos,
+        *args,
         **kwargs
     ):
-        super().__init__(*pos, **kwargs)
+        super().__init__(*args, **kwargs)
+        self.paradigm = None
+        self.tags = ()
+
+    
+    def get_inflection_table(self, wordform: str) -> List[Tuple[str, str]]:
+        # for now, assume wordform is the baseform
+        variables = morphparser.eval_baseform(self.paradigm, wordform, self.tags)
+        if variables is None:
+            return []
+        res = []
+        table = self.paradigm(*variables)
+        for form, msd in table:
+            res.append((msd, form))
+            if not msd:
+                # when can this happend?
+                res.append((None, form))
+        return res
 
 
 def create_morphological_entry(
--- karp/tests/unit_tests/domain_models_morphological_entry_unit_test.py
+++ karp/tests/unit_tests/domain_models_morphological_entry_unit_test.py
@@ -9,3 +9,17 @@ def test_morph_entry_has_entry_id():
     assert isinstance(morph_entry, Entry)
     assert isinstance(morph_entry, MorphologicalEntry)
     assert morph_entry.entry_id == "pn_test"
+
+
+def test_morph_entry_has_function_get_inflection_table():
+    morph_entry = create_morphological_entry("pn_test")
+    
+    assert getattr(morph_entry, "get_inflection_table")
+
+
+def test_morph_entry_inflect_av():
+    morph_entry = create_morphological_entry("av_1_blå")
+
+    inflection_table = morph_entry.get_inflection_table("grå")
+
+    assert inflection_table == [("pos indef sg u nom", "grå")]
