4fb9394a3b97409d781a87541c77f6c858f558bf
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2021-04-01 10:53:33 +0200

    test: add dependency on es

--- tests/integration_tests/cli_update_entries_integration_test.py
+++ tests/integration_tests/cli_update_entries_integration_test.py
@@ -1,25 +1,25 @@
 from karp import cli
 
 
-def test_entries_group_exist(cli_scope_session):
+def test_entries_group_exist(cli_scope_session, es):
     cli_runner = cli_scope_session(use_elasticsearch=True)
     result = cli_runner.invoke(cli.cli, ["entries", "--help"])
     assert result.exit_code == 0
 
 
-def test_entries_import_exist(cli_scope_session):
+def test_entries_import_exist(cli_scope_session, es):
     cli_runner = cli_scope_session(use_elasticsearch=True)
     result = cli_runner.invoke(cli.cli, ["entries", "import", "--help"])
     assert result.exit_code == 0
 
 
-def test_entries_update_exist(cli_scope_session):
+def test_entries_update_exist(cli_scope_session, es):
     cli_runner = cli_scope_session(use_elasticsearch=True)
     result = cli_runner.invoke(cli.cli, ["entries", "update", "--help"])
     assert result.exit_code == 0
 
 
-def test_entries_delete_exist(cli_scope_session):
+def test_entries_delete_exist(cli_scope_session, es):
     cli_runner = cli_scope_session(use_elasticsearch=True)
     result = cli_runner.invoke(cli.cli, ["entries", "delete", "--help"])
     assert result.exit_code == 0
