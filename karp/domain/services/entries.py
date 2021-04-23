f85a584b232ae60bc9dd967ab40107d0abb5e7b0
Kristoffer Andersson <kod.kristoff@gmail.com>
2021-04-23 15:52:22 +0200

    refactor: add domain service: entries

---
+++ karp/domain/services/entries.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from karp.domain.models.entry import Entry
+from karp.domain.models.resource import Resource
+
+from karp.infrastructure.unit_of_work import unit_of_work
+
+
+def add_entry(resource: Resource, entry: Entry) -> str:
+    return add_entries(resource, [entry])[0]
+
+
+def add_entries(resource: Resource, entries: List[Entry]) -> List[str]:
+    if resource is None:
+        raise ValueError("Must provide resource.")
+
+    entry_ids = []
+    with unit_of_work(using=resource.entry_repository) as uw:
+        for entry in entries:
+            uw.put(entry)
+            entry_ids.append(entry.entry_id)
+
+    return entry_ids
