47719da24932f53d783dfefc97aad01b63f89dd0
Kristoffer Andersson <kod.kristoff@gmail.com>
2021-06-08 08:25:15 +0200

    test: add tests for entry_query

--- karp/tests/unit/test_entry_query.py
+++ karp/tests/unit/test_entry_query.py
@@ -19,3 +19,31 @@ class TestSearchIds:
             random_refs.make_create_resource_command("existing"))
         with pytest.raises(errors.ResourceNotPublished):
             entry_query.search_ids("existing", ["entry"], bus.ctx)
+
+
+class TestQuery:
+    def test_cannot_search_non_existent_resource(self):
+        bus = bootstrap_test_app()
+        with pytest.raises(errors.ResourceNotFound):
+            entry_query.query("non_existing", ["entry"], bus.ctx)
+
+    def test_cannot_search_non_published_resource(self):
+        bus = bootstrap_test_app()
+        bus.handle(
+            random_refs.make_create_resource_command("existing"))
+        with pytest.raises(errors.ResourceNotPublished):
+            entry_query.query("existing", ["entry"], bus.ctx)
+
+
+class TestQuerySplit:
+    def test_cannot_search_non_existent_resource(self):
+        bus = bootstrap_test_app()
+        with pytest.raises(errors.ResourceNotFound):
+            entry_query.query_split("non_existing", ["entry"], bus.ctx)
+
+    def test_cannot_search_non_published_resource(self):
+        bus = bootstrap_test_app()
+        bus.handle(
+            random_refs.make_create_resource_command("existing"))
+        with pytest.raises(errors.ResourceNotPublished):
+            entry_query.query_split("existing", ["entry"], bus.ctx)
