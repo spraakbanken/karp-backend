4ae0ae41677bf0135b4bf501f255d045084b3286
Kristoffer Andersson <kristoffer.andersson@gu.se>
2021-09-02 13:43:56 +0200

    refactor: update import

--- karp/webapp/health_api.py
+++ karp/webapp/health_api.py
@@ -7,7 +7,7 @@ Used to perform readiness and liveness probes on the server.
 from fastapi import APIRouter, Response, status
 
 from karp.application import schemas
-from karp.application.services.system_monitor import check_database_status
+from karp.services import system_monitor
 
 
 router = APIRouter()
@@ -17,7 +17,7 @@ from .app_config import bus
 
 @router.get("/healthz", response_model=schemas.SystemMonitorResponse)
 def perform_health_check(response: Response):
-    db_status = check_database_status(bus.ctx)
+    db_status = system_monitor.check_database_status(bus.ctx)
     if not db_status:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     return {"database": db_status.message}
