2c078e21c62f4c32be7ef5e05b54f00c85e16c57
Kristoffer Andersson <kristoffer.andersson@gu.se>
2021-09-29 13:34:05 +0200

    style: clean

--- karp/lex/application/handlers/__init__.py
+++ karp/lex/application/handlers/__init__.py
@@ -3,10 +3,3 @@ from karp.services.entry_handlers import (AddEntryHandler, DeleteEntryHandler,
 from karp.services.resource_handlers import (CreateResourceHandler,
                                              PublishResourceHandler,
                                              UpdateResourceHandler)
-
-__all__ = [
-    'AddEntryHandler',
-    'DeleteEntryHandler',
-    'UpdateEntryHandler',
-    'CreateResourceHandler', 'PublishResourceHandler', 'UpdateResourceHandler'
-]
