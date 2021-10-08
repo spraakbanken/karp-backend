5147922a3aa06603aa415eb4f59ac6f0cc2be9eb
Kristoffer Andersson <kristoffer.andersson@gu.se>
2021-10-08 08:43:07 +0200

    refactor

--- karp/search/domain/search_service.py
+++ karp/search/domain/search_service.py
@@ -6,11 +6,7 @@ from typing import Callable, Dict, List, Optional, Tuple, TypeVar
 import attr
 import pydantic
 
-from karp.domain import errors, model
-from karp.domain.errors import ConfigurationError
-from karp.domain.models.entry import Entry
 from karp.domain.models.query import Query
-from karp.domain.models.resource import Resource
 
 logger = logging.getLogger("karp")
 
