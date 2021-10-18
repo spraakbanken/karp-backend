6353fc43a07c62d85832bd3e576a74c5dbb0a515
Kristoffer Andersson <kod.kristoff@gmail.com>
2021-10-18 11:52:21 +0200

    stub

--- karp/lex/application/repositories/entry_repository_uow.py
+++ karp/lex/application/repositories/entry_repository_uow.py
@@ -1,5 +1,8 @@
 import abc
-from typing import Dict
+from typing import Dict, Protocol
+
+import injector
+
 
 from karp.foundation.value_objects import UniqueId
 from .unit_of_work import EntryUnitOfWork
@@ -15,3 +18,33 @@ class EntryRepositoryUnitOfWorkFactory:
         config: Dict,
     ) -> EntryUnitOfWork:
         pass
+
+
+class EntryUnitOfWorkCreator(Protocol):
+    def __call__(
+        self,
+        entity_id: UniqueId,
+        name: str,
+        config: Dict,
+    ) -> EntryUnitOfWork:
+        pass
+
+
+class InjectorEntryUnitOfWorkRepoFactory(
+    EntryRepositoryUnitOfWorkFactory
+):
+    def __init__(self, container: injector.Injector) -> None:
+        self._container = container
+
+    def create(
+        self,
+        repository_type: str,
+        entity_id: UniqueId,
+        name: str,
+        config: Dict,
+    ) -> EntryUnitOfWork:
+        uow_factory = self._container.get(
+            Dict[str, EntryUnitOfWorkCreator]
+        )[repository_type]
+
+        return uow_factory(entity_id, name, config)
---
+++ karp/tests/unit/lex/test_entry_uow_factory.py
@@ -0,0 +1,4 @@
+def test_create_fake_entry_repo_uow(
+    lex_ctx,
+):
+    uow_factory = lex_ctxw
