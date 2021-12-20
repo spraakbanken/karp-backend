f742d04a39d6f4e1a1e184d6273d57e086d3d800
Kristoffer Andersson <kristoffer.andersson@gu.se>
2021-12-20 11:54:19 +0100

    test: fix sql_entries integration tests

--- karp/tests/integration/test_sql_entry_repository.py
+++ karp/tests/integration/test_sql_entry_repository.py
@@ -1,23 +1,15 @@
 import pytest
 
-# from karp.domain import repository
-# from karp.domain.models.entry import Entry, create_entry  # EntryRepository,
-# from karp.domain.value_objects import unique_id
-# from karp.infrastructure.unit_of_work import unit_of_work
-from karp.lex_infrastructure.sql.sql_entry_repository import SqlEntryRepository
+from karp.lex_infrastructure.repositories.sql_entries import SqlEntryRepository
 
 from karp.tests.unit.lex import factories
 
 
 @pytest.fixture(name="entry_repo")
 def fixture_entry_repo(sqlite_session_factory):
-    # entry_repo = EntryRepository.create(
-    #     "sql_v1", {"table_name": "test_name", "config": {}}
-    # )
-    # assert isinstance(entry_repo, SqlEntryRepository)
     session = sqlite_session_factory()
     entry_repo = SqlEntryRepository.from_dict(
-        settings={"table_name": "test_name", "resource_id": "test_name"},
+        name="test_name",
         resource_config={},
         session=session,
     )
@@ -30,10 +22,7 @@ def fixture_entry_repo(sqlite_session_factory):
 def fixture_entry_repo2(sqlite_session_factory):
     session = sqlite_session_factory()
     entry_repo = SqlEntryRepository.from_dict(
-        settings={
-            "table_name": "test_name2",
-            "resource_id": "test_name2",
-        },
+        name="test_name2",
         resource_config={},
         session=session,
     )
@@ -67,54 +56,5 @@ def test_update_entry_to_entry_repo(entry_repo):
     assert entry_repo.by_entry_id(entry.entry_id).version == entry.version
 
 
-#         uw.commit()
-
-#         assert uw.entry_ids() == ["a"]
-
-#         entry_copy = uw.by_id(entry.id)
-
-#         assert entry_copy.id == entry.id
-
-#         entry_copy_from_str = uw.by_id(str(entry.id))
-
-#         assert entry_copy_from_str.id == entry.id
-
-
 def test_create_entry_repository2(entry_repo2):
     assert entry_repo2.entry_ids() == []
-
-
-# def test_put_entry_to_entry_repo2(entry_repo2):
-#     with unit_of_work(using=entry_repo2) as uw:
-#         uw.put(create_entry("a", {}))
-#         uw.commit()
-
-#         assert uw.entry_ids() == ["a"]
-
-
-# def test_discard_entry_from_entry_repo2(entry_repo2):
-#     with unit_of_work(using=entry_repo2) as uw:
-#         entry = create_entry("b", {})
-#         previous_last_modified = entry.last_modified
-#         uw.put(entry)
-#         uw.commit()
-
-#         entry = uw.by_entry_id("b")
-
-#         assert uw.entry_ids() == ["a", "b"]
-#         entry.discard(user="Test", message="Delete.")
-#         assert entry.discarded
-#         assert entry.last_modified > previous_last_modified
-#         uw.update(entry)
-#         uw.commit()
-
-#         entry_copy = uw.by_entry_id("b")
-
-#         assert entry_copy.discarded
-
-#         entry_history = uw.history_by_entry_id("b")
-
-#         assert len(entry_history) == 2
-#         assert not entry_history[0].discarded
-#         assert entry_history[1].discarded
-#         assert uw.entry_ids() == ["a"]
