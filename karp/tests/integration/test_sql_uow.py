502147618e3ca9aa5325823b41a3327ac7240fff
Kristoffer Andersson <kristoffer.andersson@gu.se>
2021-12-21 13:27:19 +0100

    test: fix some tests

--- karp/tests/integration/test_sql_uow.py
+++ karp/tests/integration/test_sql_uow.py
@@ -1,13 +1,25 @@
 import pytest
+from karp.foundation.events import EventBus, Event
 
-from karp.lex_infrastructure.sql import sql_unit_of_work
+from karp.lex_infrastructure import SqlResourceUnitOfWork
+from karp.lex_infrastructure.repositories.sql_entries import SqlEntryUnitOfWork
 
-from karp.tests.unit.lex import factories
+from karp.tests.unit.lex import adapters, factories
+
+
+class FakeEventBus(EventBus):
+    def __init__(self) -> None:
+        super().__init__()
+        self.events = []
+
+    def post(self, event: Event) -> None:
+        self.events.append(event)
 
 
 class TestSqlResourceUnitOfWork:
     def test_rolls_back_uncommitted_work_by_default(self, sqlite_session_factory):
-        uow = sql_unit_of_work.SqlResourceUnitOfWork(sqlite_session_factory)
+        uow = SqlResourceUnitOfWork(
+            sqlite_session_factory, event_bus=FakeEventBus())
         with uow:
             resource = factories.ResourceFactory()
             uow.resources.save(resource)
@@ -17,15 +29,17 @@ class TestSqlResourceUnitOfWork:
         assert rows == []
 
     def test_rolls_back_on_error(self, sqlite_session_factory):
-        class MyException(Exception):
-            pass
 
-        uow = sql_unit_of_work.SqlResourceUnitOfWork(sqlite_session_factory)
-        with pytest.raises(MyException):
+        def do_something_that_fails(resource):
+            print(resource.to_string())
+
+        uow = SqlResourceUnitOfWork(
+            sqlite_session_factory, event_bus=FakeEventBus())
+        with pytest.raises(AttributeError):
             with uow:
-                resource = random_resource()
-                uow.resources.put(resource)
-                raise MyException()
+                resource = factories.ResourceFactory()
+                uow.resources.save(resource)
+                do_something_that_fails(resource)
 
         new_session = sqlite_session_factory()
         rows = list(new_session.execute('SELECT * FROM "resources"'))
@@ -34,14 +48,15 @@ class TestSqlResourceUnitOfWork:
 
 class TestSqlEntryUnitOfWork:
     def test_rolls_back_uncommitted_work_by_default(self, sqlite_session_factory):
-        uow = sql_unit_of_work.SqlEntryUnitOfWork(
-            {"resource_id": "abc", "table_name": "abc"},
-            resource_config={"resource_id": "abc", "config": {}},
+        uow = SqlEntryUnitOfWork(
+            # {"resource_id": "abc", "table_name": "abc"},
+            # resource_config={"resource_id": "abc", "config": {}},
             session_factory=sqlite_session_factory,
+            event_bus=FakeEventBus()
         )
         with uow:
-            entry = random_entry(resource_id="abc")
-            uow.entries.put(entry)
+            entry = factories.EntryFactory(resource_id="abc")
+            uow.entries.save(entry)
 
         new_session = sqlite_session_factory()
         rows = list(new_session.execute('SELECT * FROM "resources"'))
@@ -51,7 +66,7 @@ class TestSqlEntryUnitOfWork:
         class MyException(Exception):
             pass
 
-        uow = sql_unit_of_work.SqlEntryUnitOfWork(
+        uow = SqlEntryUnitOfWork(
             {"resource_id": "abc", "table_name": "abc"},
             resource_config={"resource_id": "abc", "config": {}},
             session_factory=sqlite_session_factory,
