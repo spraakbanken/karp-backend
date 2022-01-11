59d0825104b0ef3697362d4d972087473d8157a4
Kristoffer Andersson <kristoffer.andersson@gu.se>
2022-01-11 14:48:34 +0100

    test: update uow test

--- karp/tests/integration/test_sql_uow.py
+++ karp/tests/integration/test_sql_uow.py
@@ -1,4 +1,6 @@
 import pytest
+
+from karp.foundation.value_objects import make_unique_id
 from karp.foundation.events import EventBus, Event
 
 from karp.lex_infrastructure import SqlResourceUnitOfWork
@@ -46,37 +48,55 @@ class TestSqlResourceUnitOfWork:
         assert rows == []
 
 
+class MyException(Exception):
+    pass
+
+
 class TestSqlEntryUnitOfWork:
+
     def test_rolls_back_uncommitted_work_by_default(self, sqlite_session_factory):
         uow = SqlEntryUnitOfWork(
             # {"resource_id": "abc", "table_name": "abc"},
             # resource_config={"resource_id": "abc", "config": {}},
-            session_factory=sqlite_session_factory,
-            event_bus=FakeEventBus()
+            sqlite_session_factory,
+            event_bus=FakeEventBus(),
+            name="test",
+            config={},
+            connection_str=":memory:",
+            message="test message",
+            entity_id=make_unique_id(),
         )
         with uow:
-            entry = factories.EntryFactory(resource_id="abc")
+            entry = factories.EntryFactory()  # resource_id="abc")
             uow.entries.save(entry)
 
         new_session = sqlite_session_factory()
-        rows = list(new_session.execute('SELECT * FROM "resources"'))
+        rows = list(new_session.execute('SELECT * FROM "test"'))
         assert rows == []
 
     def test_rolls_back_on_error(self, sqlite_session_factory):
-        class MyException(Exception):
-            pass
 
         uow = SqlEntryUnitOfWork(
-            {"resource_id": "abc", "table_name": "abc"},
-            resource_config={"resource_id": "abc", "config": {}},
-            session_factory=sqlite_session_factory,
+            # {"resource_id": "abc", "table_name": "abc"},
+            # resource_config={"resource_id": "abc", "config": {}},
+            sqlite_session_factory,
+            event_bus=FakeEventBus(),
+            name="test",
+            config={},
+            connection_str=":memory:",
+            message="test message",
+            entity_id=make_unique_id(),
         )
         with pytest.raises(MyException):
             with uow:
-                entry = random_entry(resource_id="abc")
-                uow.entries.put(entry)
-                raise MyException()
+                entry = factories.EntryFactory()
+                uow.entries.save(entry)
+                do_something_that_fails()
 
         new_session = sqlite_session_factory()
-        rows = list(new_session.execute('SELECT * FROM "resources"'))
+        rows = list(new_session.execute('SELECT * FROM "test"'))
         assert rows == []
+
+
+def do_something_that_fails() -> None:
+    raise MyException()
