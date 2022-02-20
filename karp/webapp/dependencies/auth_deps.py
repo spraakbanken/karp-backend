0232f5a94aa0b959dbe330d5a69ce8223b95e602
Kristoffer Andersson <kod.kristoff@gmail.com>
2022-02-20 22:37:08 +0100

    fix: rearrange functions

--- karp/webapp/dependencies/auth_deps.py
+++ karp/webapp/dependencies/auth_deps.py
@@ -35,10 +35,34 @@ def bearer_scheme(authorization=Header(None)):
     return HTTPAuthorizationCredentials(scheme=scheme, credentials=credentials)
 
 
+def get_resource_permissions(
+    query: lex.GetPublishedResources = Depends(lex_deps.get_published_resources)
+) -> auth.GetResourcePermissions:
+    return LexGetResourcePermissions(query)
+
+# def get_resource_permissions(conn: Connection = Depends(database.get_connection)) -> GetResourcePermissions:
+#     return
+
+def get_is_resource_protected(
+    repo: lex.ReadOnlyResourceRepository = Depends(lex_deps.get_resources_read_repo),
+) -> auth.IsResourceProtected:
+    return LexIsResourceProtected(repo)
+
+
+def get_auth_service(
+    config: auth.AuthServiceConfig = Depends(inject_from_req(auth.AuthServiceConfig)),
+    query: auth.IsResourceProtected = Depends(get_is_resource_protected),
+) -> auth.AuthService:
+    return JWTAuthService(
+        pubkey_path=config.pubkey_path,
+        is_resource_protected=query,
+    )
+
+
 def get_current_user(
     security_scopes: SecurityScopes,
     credentials: Optional[HTTPAuthorizationCredentials] = Depends(bearer_scheme),
-    auth_service: auth.AuthService = Depends(get_auth_service)),
+    auth_service: auth.AuthService = Depends(get_auth_service),
 ) -> Optional[auth.User]:
     if not credentials:
         return None
@@ -68,7 +92,7 @@ get_user_optional = get_current_user
 def get_user(
     security_scopes: SecurityScopes,
     credentials: HTTPAuthorizationCredentials = Depends(auth_scheme),
-    auth_service: auth.AuthService = Depends(get_auth_service)),
+    auth_service: auth.AuthService = Depends(get_auth_service),
 ) -> auth.User:
     if security_scopes.scopes:
         authenticate_value = f'Bearer scope="{security_scopes.scope_str}"'
@@ -93,25 +117,3 @@ def get_user(
         raise credentials_exception
 
 
-def get_resource_permissions(
-    query: lex.GetPublishedResources = Depends(lex_deps.get_published_resources)
-) -> auth.GetResourcePermissions:
-    return LexGetResourcePermissions(query)
-
-# def get_resource_permissions(conn: Connection = Depends(database.get_connection)) -> GetResourcePermissions:
-#     return
-
-def get_is_resource_protected(
-    repo: lex.ReadOnlyResourceRepository = Depends(lex_deps.get_resources_read_repo),
-) -> auth.IsResourceProtected:
-    return LexIsResourceProtected(repo)
-
-
-def get_auth_service(
-    config: auth.AuthServiceConfig = Depends(inject_from_req(auth.AuthServiceConfig)),
-    query: auth.IsResourceProtected = Depends(get_is_resource_protected),
-) -> auth.AuthService:
-    return JWTAuthService(
-        pubkey_path=config.pubkey_path,
-        is_resource_protected=query,
-    )
