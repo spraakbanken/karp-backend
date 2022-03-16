5175baa25b67fc04836bfed14632044986bd4bc8
Kristoffer Andersson <kristoffer.andersson@gu.se>
2022-03-16 15:30:28 +0100

    Bump version: 6.0.18 → 6.0.19

--- karp/__init__.py
+++ karp/__init__.py
@@ -7,7 +7,7 @@ import pkg_resources
 # import werkzeug.exceptions
 
 
-__version__ = "6.0.18"
+__version__ = "6.0.19"
 
 
 # TODO handle settings correctly
--- karp/main/config.py
+++ karp/main/config.py
@@ -6,7 +6,7 @@ from starlette.config import Config
 from starlette.datastructures import Secret
 
 PROJECT_NAME = 'Karp'
-VERSION = '6.0.18'
+VERSION = '6.0.19'
 API_PREFIX = '/'
 # SECRET_KEY = config("SECRET_KEY", cast=Secret, default="CHANGEME")
 
