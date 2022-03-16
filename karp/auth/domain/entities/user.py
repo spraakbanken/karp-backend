f226afb2447da1662d4652045d16fdf0a47fb624
Kristoffer Andersson <kristoffer.andersson@gu.se>
2022-03-16 13:48:05 +0100

    refactor: log exception

--- karp/auth/domain/entities/user.py
+++ karp/auth/domain/entities/user.py
@@ -1,6 +1,9 @@
+import logging
 import typing
 
 from karp.foundation.value_objects.permission_level import PermissionLevel
+
+logger = logging.getLogger(__name__)
 
 
 class User:
@@ -19,4 +22,17 @@ class User:
         return self._identifier
 
     def has_enough_permissions(self, resource_id: str, level: PermissionLevel) -> bool:
-        return self._permissions.get(resource_id) and (self._permissions[resource_id] >= self._levels[level])
+        try:
+            return (self._permissions.get(resource_id) is not None) and (self._permissions[resource_id] >= self._levels[level])
+        except KeyError:
+            logger.exception(
+                'error checking permissions',
+                extra={
+                    'resource_id': resource_id,
+                    'level': level,
+                    'identifier': self._identifier,
+                    'levels': self._levels,
+                    'permissions': self._permissions
+                }
+            )
+            raise
