31b2bbcc4f16333aad01cd519337988548c799c4
Kristoffer Andersson <kristoffer.andersson@gu.se>
2022-03-29 09:43:56 +0200

    fix: return result from command_handler

--- karp/foundation/commands.py
+++ karp/foundation/commands.py
@@ -5,7 +5,7 @@ from typing import Any, Generic, TypeVar
 import injector
 
 
-CommandType = TypeVar('CommandType')
+CommandType = TypeVar("CommandType")
 
 
 logger = logging.getLogger(__name__)
@@ -22,20 +22,18 @@ class Command:
 
 class CommandBus(abc.ABC):
     @abc.abstractmethod
-    def dispatch(self, command: Command) -> None:
+    def dispatch(self, command: Command) -> Any:
         raise NotImplementedError
 
 
 class InjectorCommandBus(CommandBus):
-    def __init__(self, injector: injector.Injector) -> None:
-        self._injector = injector
+    def __init__(self, container: injector.Injector) -> None:
+        self._container = container
 
-    def dispatch(self, command: Command) -> None:
-        logger.info('Handling command: %s', command)
+    def dispatch(self, command: Command) -> Any:
+        logger.info("Handling command: %s", command)
         cmd_cls = type(command)
-        cmd_handler = self._injector.get(
-            CommandHandler[cmd_cls])  # type: ignore
+        cmd_handler = self._container.get(CommandHandler[cmd_cls])  # type: ignore
 
-        logger.info('Handling command %s with handler %s',
-                    command, cmd_handler)
-        cmd_handler.execute(command)
+        logger.info("Handling command %s with handler %s", command, cmd_handler)
+        return cmd_handler.execute(command)
