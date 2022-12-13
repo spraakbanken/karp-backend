6d4de906592d85446ec7cb8316632bf53cffe826
Kristoffer Andersson <kristoffer.andersson@gu.se>
2022-12-13 10:21:21 +0100

    Bump version: 6.1.0 → 6.1.1

--- karp/main/config.py
+++ karp/main/config.py
@@ -6,7 +6,7 @@ from starlette.config import Config
 from starlette.datastructures import Secret
 
 PROJECT_NAME = 'Karp'
-VERSION = '6.1.0'
+VERSION = '6.1.1'
 API_PREFIX = '/'
 # SECRET_KEY = config("SECRET_KEY", cast=Secret, default="CHANGEME")
 
