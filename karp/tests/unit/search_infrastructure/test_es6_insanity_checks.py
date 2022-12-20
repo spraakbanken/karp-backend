022933a8e7f7ea3cecea65882511e720fc8579f2
Kristoffer Andersson <kristoffer.andersson@gu.se>
2022-12-20 12:43:55 +0100

    test: add missing kwarg

--- karp/tests/unit/search_infrastructure/test_es6_insanity_checks.py
+++ karp/tests/unit/search_infrastructure/test_es6_insanity_checks.py
@@ -1,3 +1,4 @@
+import contextlib
 from karp.search_infrastructure.repositories.es6_indicies import (
     Es6Index,
     Es6IndexUnitOfWork,
@@ -6,13 +7,9 @@ from karp.search_infrastructure.repositories.es6_indicies import (
 
 class TestEs6Index:
     def test_can_instantiate_es6_index(self):
-        try:
+        with contextlib.suppress(AttributeError):
             Es6Index(None)
-        except AttributeError:
-            pass
 
     def test_can_instantiate_es6_index_uow(self):
-        try:
-            Es6IndexUnitOfWork(None, event_bus=None)
-        except AttributeError:
-            pass
+        with contextlib.suppress(AttributeError):
+            Es6IndexUnitOfWork(None, event_bus=None, index_prefix="")
