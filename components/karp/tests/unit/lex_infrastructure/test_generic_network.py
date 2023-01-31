40b3d6ef814e3a457d45614365e83af688ac16d8
Kristoffer Andersson <kristoffer.andersson@gu.se>
2023-01-31 15:08:06 +0100

    refactor: clean

--- components/karp/tests/unit/lex_infrastructure/test_generic_network.py
+++ components/karp/tests/unit/lex_infrastructure/test_generic_network.py
@@ -1,5 +1,3 @@
-from karp.lex.domain.entities.entry import create_entry
-from karp.foundation.value_objects import unique_id
 from karp.lex_infrastructure.queries import generic_network
 
 from karp.tests.unit.lex import factories
@@ -9,7 +7,6 @@ def test__create_ref():
     resource_id = "resource_id"
     resource_version = 1
     _id = 5
-    entry_id = "entry_id"
     entry_body = {"body": {"of": "entry"}}
     entry = factories.EntryFactory(body=entry_body)
 
