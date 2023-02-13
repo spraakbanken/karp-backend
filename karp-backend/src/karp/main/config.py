55dc3021a40750ef7d48e785ebf4f2adb1707a7b
Kristoffer Andersson <kod.kristoff@gmail.com>
2023-02-13 19:03:08 +0100

    Bump version: 6.2.0 → 6.2.1

--- karp-backend/src/karp/main/config.py
+++ karp-backend/src/karp/main/config.py
@@ -6,7 +6,7 @@ from starlette.config import Config  # noqa: F401
 from starlette.datastructures import Secret  # noqa: F401
 
 PROJECT_NAME = "Karp"
-VERSION = "6.2.0"
+VERSION = "6.2.1"
 API_PREFIX = "/"
 # SECRET_KEY = config("SECRET_KEY", cast=Secret, default="CHANGEME")
 
