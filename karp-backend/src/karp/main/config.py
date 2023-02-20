a78158b270b6d4327095e9208590f40bef51460a
Kristoffer Andersson <kod.kristoff@gmail.com>
2023-02-20 08:14:04 +0100

    build: revert version

--- karp-backend/src/karp/main/config.py
+++ karp-backend/src/karp/main/config.py
@@ -6,7 +6,7 @@ from starlette.config import Config  # noqa: F401
 from starlette.datastructures import Secret  # noqa: F401
 
 PROJECT_NAME = "Karp"
-VERSION = "6.3.0"
+VERSION = "6.1.12"
 API_PREFIX = "/"
 # SECRET_KEY = config("SECRET_KEY", cast=Secret, default="CHANGEME")
 
