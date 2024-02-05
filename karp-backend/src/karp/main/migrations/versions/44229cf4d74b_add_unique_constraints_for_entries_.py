9b6a703b59915690735403fb3e02c86a82f77148
Nick Smallbone <nicsma@chalmers.se>
2024-02-05 11:48:51 +0100

    lint/fmt

--- karp-backend/src/karp/main/migrations/versions/44229cf4d74b_add_unique_constraints_for_entries_.py
+++ karp-backend/src/karp/main/migrations/versions/44229cf4d74b_add_unique_constraints_for_entries_.py
@@ -5,24 +5,25 @@ Revises: e09588a7e6ca
 Create Date: 2024-02-05 11:29:56.613345
 
 """
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import mysql
 
 # revision identifiers, used by Alembic.
-revision = '44229cf4d74b'
-down_revision = 'e09588a7e6ca'
+revision = "44229cf4d74b"
+down_revision = "e09588a7e6ca"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
     conn = op.get_bind()
-    for resource_id, table_name in conn.execute(
-        sa.text("select distinct resource_id, table_name from resources")
+    for (table_name,) in conn.execute(
+        sa.text("select distinct table_name from resources")
     ):
-        op.execute(f"alter table {table_name} drop constraint if exists id_version_unique_constraint")
+        op.execute(
+            f"alter table {table_name} drop constraint if exists id_version_unique_constraint"
+        )
         op.create_unique_constraint(
-            "id_version_unique_constraint",
-            table_name,
-            ["entity_id", "version"])
+            "id_version_unique_constraint", table_name, ["entity_id", "version"]
+        )
