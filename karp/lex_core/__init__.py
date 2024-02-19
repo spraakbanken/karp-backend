aff540f1064d23666ba94c7c49e9bc053284e624
Nick Smallbone <nick@smallbone.se>
2024-02-19 11:26:20 +0100

    unused

--- karp/lex_core/__init__.py
+++ karp/lex_core/__init__.py
@@ -2,6 +2,5 @@ __version__ = "0.5.1"
 
 
 from karp.lex_core import value_objects
-from karp.lex_core.dtos import EntryDto, GenericEntryDto
 
-__all__ = ["EntryDto", "GenericEntryDto", "value_objects"]
+__all__ = ["value_objects"]
--- karp/lex_core/dtos/__init__.py
+++
@@ -1,3 +0,0 @@
-from karp.lex_core.dtos.entry_dto import EntryDto, GenericEntryDto
-
-__all__ = ["EntryDto", "GenericEntryDto"]
